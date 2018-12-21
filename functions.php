f9482840baf6e787f09f34e58f324bd5ebbbea46
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-21 00:06:42 +0000

    header and footer

--- functions.php
+++ functions.php
@@ -1,1 +1,7 @@
-<?php ?>
+<?php
+
+  function wpt_theme_styles() {
+    wp_enqueue_style( 'font', 'https://fonts.googleapis.com/css?family=Major+Mono+Display' );
+  }
+  add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
+?>
--- index.php
+++ index.php
@@ -1,2 +1,4 @@
-line 1 <?php the_field( 'heading' ); ?> 
+<?php get_header(); ?>
+line 1 <?php the_field( 'heading' ); ?>
 line 2 <?php the_field( 'sub_heading' ); ?>
+<?php get_footer(); ?>
