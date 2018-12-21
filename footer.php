8981a470f9f2aafa4992d2f7072201eebe9f1a1b
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-21 00:11:55 +0000

    header and footer

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+<?php wp_footer(); ?>
+</footer>
+<div class="arrow center-align">
+  <div class="nav animated rotateIn delay-1s">
+    <img src="assets/img/noun_arrow keys_302301.png" alt="">
+  </div>
+</div>
+</body>
+</html>
