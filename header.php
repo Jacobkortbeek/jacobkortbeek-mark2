ffc17a0cdb27bce8715ca59137048d86eb87457c
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-24 02:10:18 +0000

    home static in php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
-    <title>Jacob Kortbeek</title>
+    <title><?php wp_title( '|', true, 'right' ); bloginfo( 'name' ); ?></title>
   </head>
   <body>
     <header>
