1a7c52a124545babb8f8f2f707e40d789a11380e
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-24 02:22:34 +0000

    front page static in php

--- functions.php
+++ functions.php
@@ -3,20 +3,20 @@
   function wpt_theme_styles() {
     wp_enqueue_style( 'font', 'https://fonts.googleapis.com/css?family=Major+Mono+Display' );
     wp_enqueue_style( 'main', get_template_directory_uri() . '/style.scss'   );
-    wp_enqueue_style( 'animate', get_template_directory_uri() . '/assets/css/animate.scss' );
-    wp_enqueue_style( 'fullpage', get_template_directory_uri() . '/assets/css/fullpage.scss' );
-    wp_enqueue_style( 'material', get_template_directory_uri() . '/assets/css/material.min.scss'   );
-    wp_enqueue_style( 'materialize', get_template_directory_uri() . '/assets/css/materialize.min.scss'   );
+    wp_enqueue_style( 'animate', get_template_directory_uri() . '/css/animate.scss' );
+    wp_enqueue_style( 'fullpage', get_template_directory_uri() . '/css/fullpage.scss' );
+    wp_enqueue_style( 'material', get_template_directory_uri() . '/css/material.min.scss'   );
+    wp_enqueue_style( 'materialize', get_template_directory_uri() . '/css/materialize.min.scss'   );
   }
   add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
 
   function wpt_theme_js() {
-    wp_enqueue_script( 'fulpage', get_template_directory_uri() . '/assets/js/fullpage.js', array('jquery'), ' ', true );
-    wp_enqueue_script( 'functions', get_template_directory_uri() . '/assets/js/function.js', array('jquery'), ' ', true );
-    wp_enqueue_script( 'matchHeight', get_template_directory_uri() . '/assets/js/jquery.matchHeight.js', array('jquery'), ' ', true );
-    wp_enqueue_script( 'material', get_template_directory_uri() . '/assets/js/material.js', array('jquery'), ' ', true );
-    wp_enqueue_script( 'materialize', get_template_directory_uri() . '/assets/js/materialize.min.js', array('jquery'), ' ', true );
-    wp_enqueue_script( 'scrollFlow', get_template_directory_uri() . '/assets/js/scrolloverflow.js', array('jquery'), ' ', true );
+    wp_enqueue_script( 'fulpage', get_template_directory_uri() . '/js/fullpage.js', array('jquery'), ' ', true );
+    wp_enqueue_script( 'functions', get_template_directory_uri() . '/js/function.js', array('jquery'), ' ', true );
+    wp_enqueue_script( 'matchHeight', get_template_directory_uri() . '/js/jquery.matchHeight.js', array('jquery'), ' ', true );
+    wp_enqueue_script( 'material', get_template_directory_uri() . '/js/material.js', array('jquery'), ' ', true );
+    wp_enqueue_script( 'materialize', get_template_directory_uri() . '/js/materialize.min.js', array('jquery'), ' ', true );
+    wp_enqueue_script( 'scrollFlow', get_template_directory_uri() . '/js/scrolloverflow.js', array('jquery'), ' ', true );
   }
   add_action( 'wp_enqueue_scripts', 'wpt_theme_js' );
 ?>
